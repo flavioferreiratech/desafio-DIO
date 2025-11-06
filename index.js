6ca248f8f4bcf43fa779b15f67b3686a804f9222
flavioferreiratech <flaviobrah.cybereng@gmail.com>
2025-11-06 00:08:06 -0300

    atualização de projeto

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-console.log ("ola")
-let nome = "flavio"
-console.log("nome")
+let NomeDoHeroi = "Heroi Flavio"
+console.log ("olá "  + NomeDoHeroi) 
+console.log("Seja bem vindo a programação")
+let Xp = 9000
+console.log("Atualmente o nível do seu heroi é " + Xp)
+if (Xp <=1000) { 
+    console.log(NomeDoHeroi + ", Sua classificação é Ferro")}
+    if (Xp >= 1001 && Xp <=2000) {
+    console.log(NomeDoHeroi + ", Sua classificação é Bronze")}
+    if (Xp >= 2001 && Xp<=5000) {
+    console.log(NomeDoHeroi + ", Sua classificação é Prata")}
+    if (Xp >= 5001 && Xp<=7000) {
+    console.log(NomeDoHeroi + ", Sua classificação é Ouro")}
+    if (Xp >= 7001 && Xp<=8000) {
+    console.log(NomeDoHeroi + ", Sua classificação é Platina")}
+    if (Xp >= 8001 && Xp<=9000) {
+    console.log(NomeDoHeroi + ", Sua classificação é Ascendente")}
+    if (Xp >= 9001 && Xp<=10000) {
+    console.log(NomeDoHeroi + ", Sua classificação é imortal")}
+    if (Xp >= 10001) {
+    console.log(NomeDoHeroi + ", Sua classificação é Radiante")}
+
+    
+    
